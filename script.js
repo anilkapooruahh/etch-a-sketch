870d14bc0e3705a000167eda8d09b266e5f0bdd4
Shreyan Das <hrijudas@hotmail.com>
2022-05-07 18:01:37 +0530

    Rainbow Mode

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 //CONSTANTS
 const EDGE = 16
-const DIVS =  EDGE * EDGE // 16 * 16
+const DIVS = EDGE * EDGE // 16 * 16
 const height = window.innerHeight
-const width = window.innerWidth
-const limit = (width < height ? width : height) 
-const side = Math.floor(limit / EDGE)
+let RAINBOW = false
 
+const createRandomHex = () => Math.floor(Math.random() * 256)
+const createColorHex = () => `rgb(${createRandomHex()}, ${createRandomHex()}, ${createRandomHex()})`
 
+// Event Listeners
+const handleMouseOver = (element) => {
+    if (RAINBOW === true) {
+        element.style.backgroundColor = createColorHex()
+    } else {
+        element.classList.add('hover')
+    }
+}
+
+rainbowButton = document.querySelector('button')
+const handleRainbowClick = () => { 
+    if (RAINBOW === true) {
+        RAINBOW = false
+    } else {
+        RAINBOW = true
+    }
+}
+rainbowButton.addEventListener('click', () => {handleRainbowClick()})
+rainbowButton.style.backgroundColor = 'blue'
+// making the grid settings
 const body = document.querySelector('div.container')
+body.style.gridTemplateColumns = `repeat(${EDGE}, 1fr)`
+body.style.gridTemplateRows = `repeat(${EDGE}, 1fr)`
 
+//Drawing the grid
 for (let i = 0; i < DIVS; i++) {
-    const element =  document.createElement('div')
-    element.textContent = `${i}`
+    const element = document.createElement('div')
     element.classList.add("grid-element")
-    element.style.setProperty("--grid-element-size", `${side}px`)
+    // Event Handling 
+    element.addEventListener('mouseover', () => handleMouseOver(element))
+    
     body.appendChild(element)
 }
